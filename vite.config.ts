d1dfe04392db218908941648db1f718a3a3fdd0d
jrjunior72 <jrjunior72@hotmail.com>
2025-05-23 19:16:30 -0300

    fix: resolve erro de build no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint' // Sintaxe pré-v2.0
+// import eslint from 'vite-plugin-eslint' // Sintaxe pré-v2.0
 
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'], // Ordem de resolução
     alias: {
